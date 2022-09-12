d1a727c4392172596d018b9b6d8d1fb903598f0e
anthony-cabacungan <antcabacungan@csu.fullerton.edu>
2022-09-12 10:15:59 -0700

    used create-react-app for necessary components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Trivia Game</h1>
+
+      <p>What is the mascot of CSUF</p>
+
+      <button>Click me</button>
+      <button>Click me</button>
+      <button>Click me</button>
+      <button>Click me</button>
+    </div>
+  );
+}
+
+export default App;
