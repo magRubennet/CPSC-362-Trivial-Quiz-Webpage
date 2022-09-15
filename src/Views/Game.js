452aa27d08308db857fe7e2525841660fd2e4105
anthony-cabacungan <antcabacungan@csu.fullerton.edu>
2022-09-15 16:06:35 -0700

    added game.js

---
+++ src/Views/Game.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Game() {
+    return (
+        <div> 
+            <h1>Trivia Game</h1>
+
+            <p>What is the mascot of CSUF</p>
+
+            <button>Click me</button>
+            <button>Click me</button>
+            <button>Click me</button>
+            <button>Click me</button>
+        </div>
+    );
+}
