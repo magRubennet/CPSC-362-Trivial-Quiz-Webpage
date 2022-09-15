5dc0475cce1f05748181674adfc60b4b0c1e601f
anthony-cabacungan <antcabacungan@csu.fullerton.edu>
2022-09-15 16:06:08 -0700

    modifed app file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import React from 'react';
 import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Views/Home";
+import Game from "./Views/Game";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Trivia Game</h1>
+    <>
+      <Navbar />
+      <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/game" element={<Game />}></Route>
 
-      <p>What is the mascot of CSUF</p>
-
-      <button>Click me</button>
-      <button>Click me</button>
-      <button>Click me</button>
-      <button>Click me</button>
-    </div>
+      </Routes>
+      </div>
+    </>
   );
 }
 
