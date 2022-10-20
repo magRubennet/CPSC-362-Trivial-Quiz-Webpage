13033ecd4621e1230d1b34b3b62bd58b85fd9843
Anthony Cabacungan <antcabacungan@csu.fullerton.edu>
2022-10-19 23:09:02 -0700

    added finalscore.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import Home from "./Views/Home";
 import Game from "./Views/Game";
 import GameSetup from "./Views/GameSetup";
+import FinalScore from "./Views/FinalScore"
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/GameSetup" element={<GameSetup />}></Route>
           <Route path="/Game" element={<Game />}></Route>
+          <Route path="/FinalScore" element={<FinalScore />}></Route>
         </Routes>
       </div>
     </>
